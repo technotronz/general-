e4442f561bd4411d4ccccde29801412274cd0e45
TZ'23 <123309049+technotronz@users.noreply.github.com>
2023-01-25 10:44:09 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -22,7 +22,7 @@ hide_ststyle = """
             header {visibility: hidden;}
             </style>
             """
-st.write("test")
+st.write("test1")
 html_gr='''
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
