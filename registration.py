a43fbf1c1a7d5b75ff00d04aa903d8a7b3869aac
TZ'23 <123309049+technotronz@users.noreply.github.com>
2023-01-30 14:30:28 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -999,6 +999,7 @@ year=st.selectbox("Year of study: ",options=["--Choose--","I","II","III","IV","V
 ph=st.text_input('Your mobile number (follow this format - without country code: 935xxxxxxx): ')
 
 def check(email):
+			email = email.strip()
             if not (re.fullmatch(regex, email)):
                         return 1
             return 0
